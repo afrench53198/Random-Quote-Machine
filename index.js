af54a28152782b43f9fb7cf5a623d00cee1cacc9
Adam French <Afrench53198@gmail.com>
2018-03-14 13:08:56 -0700

    Started configuring sharing

--- index.js
+++ index.js
@@ -2,31 +2,19 @@
 
 window.addEventListener("DOMContentLoaded", function() {
   
-  
-    
-
     var container = document.querySelector(".container");
     var header = document.querySelector("#quote-text");
     var quoteContent = document.querySelector("#quote-content");
-    var innerQuoteContent = document.querySelector("#inner-quote-content");
 
     var randomQuoteMachine = {
-        previousQuotes:[],
-        currentQuote:{},
+        currentQuote: {},
         url:"http://quotesondesign.com/wp-json/posts?filter[orderby]=rand",
         handleQuote: function(response) {
-            header.innerHTML = ''
-            if (this.currentQuote) {
-                this.previousQuotes.push(this.currentQuote);
-            }
+            header.innerHTML = '';
             this.currentQuote = response;
-        
             if (quoteContent.childElementCount > 0) {
-                var nextButton = document.querySelector("#next");
+                var innerQuoteContent = document.querySelector("#inner-quote-content");
                 quoteContent.removeChild(innerQuoteContent);
-                document.querySelector("#nxt").className ="fas fa-arrow-right";
-                nextButton.style.gridColumn = '2/-1';
-                document.querySelector("#previous").style.display = "block";
             } 
             var quoteHtml = `
             <div id="inner-quote-content">
@@ -42,6 +30,7 @@ window.addEventListener("DOMContentLoaded", function() {
                 if (request.status === 200) {
                     var response = JSON.parse(request.responseText)[0];
                     randomQuoteMachine.handleQuote(response);
+                    request.abort();
                 }
             } else if (request.state === XMLHttpRequest.DONE && request.status != 200) {
                 console.log(request.responseText);
@@ -50,33 +39,25 @@ window.addEventListener("DOMContentLoaded", function() {
         // Get request with caching disabled
            request.open("GET",randomQuoteMachine.url + ((/\?/).test(randomQuoteMachine.url) ? "&" : "?") + (new Date()).getTime()),true;
            request.send();
-      },
-      showPreviousQuote: function() {
-        var currentQuote = this.previousQuotes.pop();
-      },
-      share: function(platform) {
-          if (platform === "facebook") {
-            FB.ui({
-               method: "share",
-               href: this.currentQuote["link"],
-            }, function(response){
-                if (response && response.post_id) {
-                    alert('success');
-                } else {
-                    alert('error');
-                }
-            });
-          } else if (platform === "twitter") {
-
-          }
-        
-      }
+    },
+        share: function(platform) {
+            var currentQuoteText = ""
+            var author = ""
+            if (document.querySelector("#inner-quote-content")) {
+                currentQuoteText = document.querySelector("#inner-quote-content").querySelector("p").innerHTML;
+                author = document.querySelector("#inner-quote-content").querySelector("h1").innerHTML;
+            }
+            if (platform == "facebook" )  {
+                var shareUrl = "//www.facebook.com/dialog/share?app_id=1790769074308211&href=" + currentQuoteText +  "-" +  author;
+                window.open(shareUrl);
+            } else if (platform == "twitter") {
+                var shareUrl = 'https://twitter.com/intent/tweet?text=' + currentQuoteText +  "-" +  author; 
+                window.open(shareUrl);
+        }
     }
+}
    
-    
-    var getQuoteButton = document.querySelector("#next");
-    getQuoteButton.addEventListener("click", randomQuoteMachine.getRandomQuote);
-
+    document.querySelector("#next").addEventListener("click", randomQuoteMachine.getRandomQuote);
     document.querySelector(".fa-facebook-f").addEventListener("click",randomQuoteMachine.share("facebook"));
-
+    document.querySelector(".fa-twitter").addEventListener("click",randomQuoteMachine.share("twitter"));
 });
